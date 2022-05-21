4c00c4be79b9143921d47a2c032c6901e8404338
Oliver Giles <ogiles@email.com>
2022-05-21 11:55:40 +0100

    working template

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-from setuptools import Extension
-from src.my_numba_package.haversine import cc
-
-if __name__ == "__main__":
-    setup(
-        ext_modules=[cc.distutils_extension()],
-    )
