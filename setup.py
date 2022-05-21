b2a7d3941ba400b05773f663e236e2dc6378636b
Oliver Giles <ogiles@email.com>
2022-05-21 11:32:58 +0100

    updated build system to pep 517 suggestions

--- setup.py
+++ setup.py
@@ -4,17 +4,6 @@ from setuptools import Extension
 from src.my_module.haversine import cc
 
 if __name__ == "__main__":
-    with open("requirements.txt") as f:
-        content = f.readlines()
-    requirements = [x.strip() for x in content if "git+" not in x]
-
     setup(
-        name="haversine_vectorize",
-        version="1.0",
-        description="Project Description",
-        packages=find_packages(),
-        install_requires=requirements,
-        include_package_data=True,
-        zip_safe=False,
         ext_modules=[cc.distutils_extension()],
     )
