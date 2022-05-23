4f2598d60946a0cd5001cfec0a11dc45ad17eda3
Oliver Giles <ogiles@email.com>
2022-05-23 13:54:02 +0100

    updated haversine.py

--- src/my_numba_package/haversine.py
+++ src/my_numba_package/haversine.py
@@ -1,9 +1,13 @@
 from numba import jit
 from numba.pycc import CC
 from math import radians, sin, cos, asin, sqrt
+import pathlib
+
+compile_path = pathlib.Path(__file__).parent.resolve()
 
 cc = CC("compiled_haversine")
 cc._source_module = "my_numba_package.haversine"
+cc.output_dir = compile_path
 
 @cc.export("fhaversine", "f8(f8,f8,f8,f8)")
 def jit_haversine(lon1,lat1,lon2,lat2):
@@ -14,4 +18,5 @@ def jit_haversine(lon1,lat1,lon2,lat2):
     return 2 * 6371 * asin(sqrt(a))
 
 if __name__ == "__main__":
+    print(compile_path)
     cc.compile()
