b4c89faf9e735edea1c93ea483fdf648135d06b3
Oliver Giles <ogiles@email.com>
2022-05-23 13:53:55 +0100

    updated setup to sym link src

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import find_packages, setup, Extension
+from setuptools import setup, Extension
 from src.my_numba_package.haversine import cc
 
 if __name__ == "__main__":
-    cc.compile()
     with open("requirements.txt") as f:
         content = f.readlines()
     requirements = [x.strip() for x in content if "git+" not in x]
 
     setup(
         name="my_numba_package",
-        version="1.0",
+        version="0.0.0",
         description="Project Description",
-        packages=find_packages(),
+        packages=["my_numba_package"],
+        package_dir={"":"src"},
         install_requires=requirements,
         include_package_data=True,
         zip_safe=False,
